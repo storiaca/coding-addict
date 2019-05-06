3200afd35f3066fe46ed5a48082765b0b1e3b9c3
storiaca <storiaca@gmail.com>
2019-05-06 11:47:06 +0200

    Create getProducts method

--- shopping-cart/app.js
+++ shopping-cart/app.js
@@ -11,7 +11,24 @@ const productsDOM = document.querySelector(".products-center");
 let cart = [];
 
 // Getting the products
-class Products {}
+class Products {
+  async getProducts() {
+    try {
+      let result = await fetch("products.json");
+      let data = await result.json();
+      let products = data.items;
+      products = products.map(item => {
+        const { title, price } = item.fields;
+        const { id } = item.sys;
+        const image = item.fields.image.fields.file.url;
+        return { title, price, id, image };
+      });
+      return products;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
 
 // Display products
 class UI {}
@@ -22,4 +39,7 @@ class Storage {}
 document.addEventListener("DOMContentLoaded", () => {
   const ui = new UI();
   const products = new Products();
+
+  // Get all products
+  products.getProducts().then(data => console.log(data));
 });
