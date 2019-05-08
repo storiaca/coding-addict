8d7db341057cc05e41c05590913149b2abd09673
Aleksandar Ristic <storiaca@gmail.com>
2019-05-08 12:57:02 +0200

    Add cart to local storage

--- shopping-cart/app.js
+++ shopping-cart/app.js
@@ -74,10 +74,10 @@ class UI {
         event.target.disabled = true;
         // get product from products
         let cartItem = {...Storage.getProduct(id), amount: 1};
-        console.log(cartItem);
-        
         // add product to the cart
+        cart = [...cart, cartItem];        
         // save cart in local storage
+        Storage.saveCart(cart)
         // set cart values
         // display cart item
         // show the cart
@@ -95,6 +95,10 @@ class Storage {
   static getProduct(id) {
     let products = JSON.parse(localStorage.getItem("products"));
     return products.find(product => product.id === id)
+  }
+
+  static saveCart(cart) {
+    localStorage.setItem("cart", JSON.stringify(cart));
   }
 }
 
