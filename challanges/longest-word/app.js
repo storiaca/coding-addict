7cb6838a26fb0bd1dc0b79a5a60f765b413fd3a2
Aleksandar Ristic <storiaca@gmail.com>
2019-05-08 12:16:33 +0200

    Longest word with forEach

--- challanges/longest-word/app.js
+++ challanges/longest-word/app.js
@@ -6,11 +6,17 @@ function longestWord(str) {
   let words = str.split(" ");
   let longestWord = "";
 
-  for(let word of words) {
+  // for(let word of words) {
+  //   if(word.length > longestWord.length) {
+  //     longestWord = word;
+  //   }
+  // }
+
+  words.forEach(word => {
     if(word.length > longestWord.length) {
       longestWord = word;
     }
-  }
+  });
   return longestWord;
 }
 
