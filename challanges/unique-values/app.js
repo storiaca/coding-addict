dce206c60d1a9d785b1b4d07186475612a2d3832
Aleksandar Ristic <storiaca@gmail.com>
2019-05-10 14:35:03 +0200

    Challanges: Unique values-Array

---
+++ challanges/unique-values/app.js
@@ -0,0 +1,20 @@
+/**
+ * Return only if all values are unique, otherwise false
+ * Using Array, Object, string method lastIndexOf()
+ * no Set Data Structure
+ */
+function unique(str) {
+  let values = [];
+  for(let letter of str) {
+    if(values.indexOf(letter) !== -1) {
+      return false;
+    } 
+    values.push(letter);
+  }
+  return true;
+}
+
+console.log(unique("abcde"));
+// true
+console.log(unique("abacdefb"));
+// false
