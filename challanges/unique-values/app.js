cae8fdbc90d1b4fa9165f4c629535f72ff1400ec
Aleksandar Ristic <storiaca@gmail.com>
2019-05-10 15:01:50 +0200

    Challanges: Unique values-string method lastIndexOf()

--- challanges/unique-values/app.js
+++ challanges/unique-values/app.js
@@ -4,14 +4,21 @@
  * no Set Data Structure
  */
 function unique(str) {
-  let values = {};
+  for (let i = 0; i < str.length; i++) {
+    if(str.lastIndexOf(str[i]) !== i) {
+      return false;
+    }    
+  }
+  return true
+  // Object type
+ /* let values = {};
   for(let letter of str) {
     if(values[letter]) {
       return false;
     } 
     values[letter] = "exists";
   }
-  return true;
+  return true; */
 
   // Array type
   // let values = [];
