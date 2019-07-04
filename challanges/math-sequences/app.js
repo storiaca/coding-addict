b5e6198081bf03b824078b6a75e050786dcfcbe0
Aleksandar Ristic <storiaca@gmail.com>
2019-07-04 10:13:22 +0200

    Email template

--- challanges/math-sequences/app.js
+++ challanges/math-sequences/app.js
@@ -7,20 +7,38 @@ function mathSequences(arr) {
   let arith = new Set();
   let geo = new Set();
 
-  for(let i = 1; i < arr.length; i++) {
-    let number1 = arr[i] - arr[i-1];
-    arith.add(number1)
-    let number2 = arr[i] / arr[i-1];
-    geo.add(number2)
+  for (let i = 1; i < arr.length; i++) {
+    let number1 = arr[i] - arr[i - 1];
+    arith.add(number1);
+    let number2 = arr[i] / arr[i - 1];
+    geo.add(number2);
   }
-  if(arith.size === 1) {
-    return "Arithmetic"
+  if (arith.size === 1) {
+    return "Arithmetic";
   }
-  if(geo.size === 1) {
-    return "Geometric"
+  if (geo.size === 1) {
+    return "Geometric";
   }
 
   return -1;
+
+  /**
+   * Example without Set()
+   */
+  // let arith = [];
+  // let geo = [];
+  // for (let i = 1; i < arr.length; i++) {
+  //   let number1 = arr[i] - arr[i - 1];
+  //   arith.push(number1)
+  //   let number2 = arr[i] / arr[i - 1];
+  //   geo.push(number2);
+  // }
+  // if (arith.every(a => a === arith[0])) {
+  //   return "Arithemtic"
+  // } else if (geo.every(g => g === geo[0])) {
+  //   return "Geometric"
+  // }
+  // return -1;
 }
 
 console.log(mathSequences([2, 4, 6, 8]));
--- challanges/regex-palindrome/app.js
+++ challanges/regex-palindrome/app.js
@@ -2,13 +2,17 @@
  * Regex Palindrome
  */
 function checkPalindrome(str) {
-  str = str.toLowerCase();
-  let first = str.split(" ").join("");
-  let second = first
+  let tempStr = str
+    .match(/[a-z0-9]+/gi)
+    .join("")
+    .toLowerCase();
+  //console.log(tempStr);
+  let second = tempStr
     .split("")
     .reverse()
     .join("");
-  return first == second;
+  //console.log(second);
+  return tempStr === second;
 }
 
 console.log(checkPalindrome("Was it a car or a cat I saw"));
